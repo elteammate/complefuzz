560a2cf91a9ab6acc8a607dbca2d08ec2424d884
elteammate <elteammate@gmail.com>
2025-03-13 15:58:57 +0300

    Add SootUp

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,17 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    api("org.soot-oss:sootup.core:2.0.0")
+    api("org.soot-oss:sootup.java.core:2.0.0")
+    api("org.soot-oss:sootup.java.sourcecode.frontend:2.0.0")
+    api("org.soot-oss:sootup.java.bytecode.frontend:2.0.0")
+    api("org.soot-oss:sootup.jimple.frontend:2.0.0")
+    api("org.soot-oss:sootup.apk.frontend:2.0.0")
+    api("org.soot-oss:sootup.callgraph:2.0.0")
+    api("org.soot-oss:sootup.analysis.intraprocedural:2.0.0")
+    api("org.soot-oss:sootup.analysis.interprocedural:2.0.0")
+    api("org.soot-oss:sootup.qilin:2.0.0")
+    api("org.soot-oss:sootup.codepropertygraph:2.0.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package org.example
 
+import sootup.core.cache.provider.LRUCacheProvider
+import sootup.java.bytecode.frontend.inputlocation.DownloadJarAnalysisInputLocation
+import sootup.java.core.views.JavaView
+
+
 fun main() {
-    println("Hello World!")
+    val inputLocations = DownloadJarAnalysisInputLocation(
+        "https://repo1.maven.org/maven2/org/apache/commons/commons-text/1.13.0/commons-text-1.13.0.jar",
+        emptyList(),
+        emptyList(),
+    )
+    val view = JavaView(listOf(inputLocations), LRUCacheProvider(1000))
+
+    val classToCreate = view.identifierFactory
+        .getClassType("org.apache.commons.text.StringSubstitutor")
+
+    val sootClass = view.getClass(classToCreate).get()
+
+    val constructors = sootClass.methods
+        .filter { it.name == "<init>" }
+        .map { it.body. }
+
+    println(constructors.joinToString("\n"))
 }
